7e937e6c878f77eda7a424d713137711f976cb64
Bits <bigbits@hackx.cc>
2021-02-03 22:29:45 +0800

    test

--- DaydreamController.js
+++ DaydreamController.js
@@ -2,118 +2,167 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-function DaydreamController() {
+function DaydreamController () {
 
 	var state = {};
 
-	function connect() {
+	function connect () {
 
-		return navigator.bluetooth.requestDevice( {
-			filters: [ {
+		return navigator.bluetooth.requestDevice({
+			filters: [{
 				name: 'mojing-motion'
-			} ],
+			}],
 			optionalServices: ['622f5b2a-60da-4484-929a-69e3a9140258']
-		} )
-		.then( function ( device ) {
-			return device.gatt.connect();
-		} )
-		.then( function ( server ) {
-			return server.getPrimaryService( '622f5b2a-60da-4484-929a-69e3a9140258' );
-		} )
-		.then( function ( service ) {
-			return service.getCharacteristic( 'ffb4a651-c224-420b-a134-13d3aba313fb' );
-		} )
-		.then( function ( characteristic ) {
-			characteristic.addEventListener( 'characteristicvaluechanged', handleData );
-			return characteristic.startNotifications();
-		} )
+		})
+			.then(function (device) {
+				return device.gatt.connect();
+			})
+			.then(function (server) {
+				return server.getPrimaryService('622f5b2a-60da-4484-929a-69e3a9140258');
+			})
+			.then(function (service) {
+				return service.getCharacteristic('ffb4a651-c224-420b-a134-13d3aba313fb');
+			})
+			.then(function (characteristic) {
+				characteristic.addEventListener('characteristicvaluechanged', handleData);
+				return characteristic.startNotifications();
+			})
 
 	}
 
-	function handleData( event ) {
+	function parseInt (original, valid_bit_length) {
+		var result = (original << (32 - valid_bit_length)) >> (32 - valid_bit_length)
+		return result
+	}
+
+	function getOrigentation (paramInt) {
+		return paramInt / Math.sqrt(2.0) / 8191.0;
+	}
+
+	function getGravity (paramInt) {
+		return 9.8 * 8.0 * paramInt / 4095.0;
+	}
+
+	function getGyro (paramInt) {
+		return 2048.0 * Math.PI * paramInt / 4095.0 / 180;
+	}
+
+	var lastTimestamp = 0;
+	var F = 0;
+
+
+	function handleData (event) {
 
 		var data = event.target.value;
 
-		var accScale = document.getElementById('accScale').value;
-		var gyroScale = document.getElementById('gyroScale').value;
+		var info = {};
+		var state = {};
+		info.timestamp = ((data.getUint8(0) & 0xFF) << 1) | ((data.getUint8(1) & 0x80) >> 7);
 
-		// http://stackoverflow.com/questions/40730809/use-daydream-controller-on-hololens-or-outside-daydream/40753551#40753551
+		info.idx = (data.getUint8(1) & 0x60) >> 5;
 
-		state.isClickDown = (data.getUint8(18) & 0x1) > 0;
-		state.isAppDown = (data.getUint8(18) & 0x4) > 0;
-		state.isHomeDown = (data.getUint8(18) & 0x2) > 0;
-		state.isVolPlusDown = (data.getUint8(18) & 0x10) > 0;
-		state.isVolMinusDown = (data.getUint8(18) & 0x8) > 0;
+		info.orientation1 = ((data.getUint8(1) & 0x31) << 9) | ((data.getUint8(2) & 0xff) << 1) | ((data.getUint8(3) & 0x80) >> 7);
+		info.orientation1 = parseInt(info.orientation1, 14);
 
-		state.time = ((data.getUint8(0) & 0xFF) << 1 | (data.getUint8(1) & 0x80) >> 7);
-		// console.log(state.time)
+		info.orientation2 = ((data.getUint8(3) & 0x7f) << 7) | ((data.getUint8(4) & 0xfe) >> 1);
+		info.orientation2 = parseInt(info.orientation2, 14);
 
-		state.seq = (data.getUint8(1) & 0x7C) >> 2;
+		info.orientation3 = ((data.getUint8(4) & 0x01) << 13) | ((data.getUint8(5) & 0xff) << 5) | ((data.getUint8(6) & 0xf8) >> 3);
+		info.orientation3 = parseInt(info.orientation3, 14);
 
-		state.xOri = (data.getUint8(1) & 0x03) << 11 | (data.getUint8(2) & 0xFF) << 3 | (data.getUint8(3) & 0x80) >> 5;
-		state.xOri = (state.xOri << 19) >> 19;
-		// state.xOri *= (2 * Math.PI / 4095.0);
-		state.xOri *= 0.000488;
+		info.accel1 = ((data.getUint8(6) & 0x07) << 10) | ((data.getUint8(7) & 0xff) << 2) | ((data.getUint8(8) & 0xc0) >> 6);
+		info.accel1 = parseInt(info.accel1, 13);
 
-		state.yOri = (data.getUint8(3) & 0x1F) << 8 | (data.getUint8(4) & 0xFF);
-		state.yOri = (state.yOri << 19) >> 19;
-		// state.yOri *= (2 * Math.PI / 4095.0);
-		state.yOri *= 0.000488;
+		info.accel2 = ((data.getUint8(8) & 0x3F) << 7) | ((data.getUint8(9) & 0xFE) >> 1);
+		info.accel2 = parseInt(info.accel2, 13);
 
-		state.zOri = (data.getUint8(5) & 0xFF) << 5 | (data.getUint8(6) & 0xF8) >> 3;
-		state.zOri = (state.zOri << 19) >> 19;
-		// state.zOri *= (2 * Math.PI / 4095.0);
-		state.zOri *= 0.000488;
+		info.accel3 = ((data.getUint8(9) & 0x01) << 12) | ((data.getUint8(10) & 0xFF) << 4) | ((data.getUint8(11) & 0xF0) >> 4);
+		info.accel3 = parseInt(info.accel3, 13);
 
-		state.xAcc = (data.getUint8(6) & 0x07) << 10 | (data.getUint8(7) & 0xFF) << 2 | (data.getUint8(8) & 0xC0) >> 6;
-		state.xAcc = (state.xAcc << 19) >> 19;
-		// state.xAcc *= (accScale * 9.8 / 4095.0);
-		state.xAcc *= 0.000488;
+		info.gyro1 = ((data.getUint8(11) & 0x0F) << 9 | (data.getUint8(12) & 0xFF) << 1 | (data.getUint8(13) & 0x80) >> 7);
+		info.gyro1 = parseInt(info.gyro1, 13);
 
+		info.gyro2 = ((data.getUint8(13) & 0x7F) << 6 | (data.getUint8(14) & 0xFC) >> 2);
+		info.gyro2 = parseInt(info.gyro2, 13);
 
-		state.yAcc = (data.getUint8(8) & 0x3F) << 7 | (data.getUint8(9) & 0xFE) >>> 1;
-		state.yAcc = (state.yAcc << 19) >> 19;
-		// state.yAcc *= (accScale * 9.8 / 4095.0);
-		state.yAcc *= 0.000488;
+		info.gyro3 = ((data.getUint8(14) & 0x03) << 11 | (data.getUint8(15) & 0xFF) << 3 | (data.getUint8(16) & 0xE0) >> 5);
+		info.gyro3 = parseInt(info.gyro3, 13);
 
-		state.zAcc = (data.getUint8(9) & 0x01) << 12 | (data.getUint8(10) & 0xFF) << 4 | (data.getUint8(11) & 0xF0) >> 4;
-		state.zAcc = (state.zAcc << 19) >> 19;
-		// state.zAcc *= (accScale * 9.8 / 4095.0);
-		state.zAcc *= 0.000488;
+		info.touchpadX = ((data.getUint8(16) & 0x1F) << 3 | (data.getUint8(17) & 0xE0) >> 5)
+		info.touchpadY = ((data.getUint8(17) & 0x1F) << 3 | (data.getUint8(18) & 0xE0) >> 5)
 
+		info.keyState = (data.getUint8(18) & 0x1f) | (data.getUint8(19) & 0x80) >> 2;
 
-		state.xGyro = ((data.getUint8(11) & 0x0F) << 9 | (data.getUint8(12) & 0xFF) << 1 | (data.getUint8(13) & 0x80) >> 7);
-		state.xGyro = (state.xGyro << 19) >> 19;
-		// state.xGyro *= (gyroScale / 180 * Math.PI / 4095.0);
-		state.xGyro *= 0.000488;
+		var f1 = getOrigentation(info.orientation1);
+		var f2 = getOrigentation(info.orientation2);
+		var f3 = getOrigentation(info.orientation3);
+		var f4 = Math.sqrt(1.0 - f1 * f1 - f2 * f2 - f3 * f3);
 
+		switch (info.idx) {
+			case 0:
+				state.xOri = f4;
+				state.yOri = f1;
+				state.zOri = f2;
+				state.wOri = f3;
+				break;
+			case 1:
+				state.xOri = f1;
+				state.yOri = f4;
+				state.zOri = f2;
+				state.wOri = f3;
+				break;
+			case 2:
+				state.xOri = f1;
+				state.yOri = f2;
+				state.zOri = f4;
+				state.wOri = f3;
+				break;
+			case 3:
+				state.xOri = f1;
+				state.yOri = f2;
+				state.zOri = f3;
+				state.wOri = f4;
+				break;
+		}
+		state.xAcc = getGravity(info.accel1);
+		state.yAcc = getGravity(info.accel2);
+		state.zAcc = getGravity(info.accel3);
+		state.xGyro = getGyro(info.gyro1)
+		state.yGyro = getGyro(info.gyro2)
+		state.zGyro = getGyro(info.gyro3)
 
-		state.yGyro = ((data.getUint8(13) & 0x7F) << 6 | (data.getUint8(14) & 0xFC) >> 2);
-		state.yGyro = (state.yGyro << 19) >> 19;
-		// state.yGyro *= (gyroScale / 180 * Math.PI / 4095.0);
-		state.yGyro *= 0.000488;
+		if (info.touchpadX != 0 || info.touchpadY != 0) {
+			state.isTouched = true;
+		} else {
+			state.isTouched = false;
+		}
 
+		state.xTouch = info.touchpadX / 255.0;
+		state.yTouch = info.touchpadY / 255.0;
 
-		state.zGyro = ((data.getUint8(14) & 0x03) << 11 | (data.getUint8(15) & 0xFF) << 3 | (data.getUint8(16) & 0xE0) >> 5);
-		state.zGyro = (state.zGyro << 19) >> 19;
-		// state.zGyro *= (gyroScale / 180 * Math.PI / 4095.0);
-		state.zGyro *= 0.000488;
+		state.isClickDown = (info.keyState & 0x1) > 0;
+		state.isAppDown = (info.keyState & 0x4) > 0;
+		state.isHomeDown = (info.keyState & 0x2) > 0;
+		state.isVolPlusDown = (info.keyState & 0x10) > 0;
+		state.isVolMinusDown = (info.keyState & 0x8) > 0;
+		state.isClickBack = (info.keyState & 0x20) > 0;
 
+		if (info.timestamp <= lastTimestamp)
+			F = 512 + F;
+		lastTimestamp = info.timestamp;
+		state.timeStamp = 1000000.0 * (F + info.timestamp);
 
-		state.xTouch = ((data.getUint8(16) & 0x1F) << 3 | (data.getUint8(17) & 0xE0) >> 5) / 255.0;
-		state.yTouch = ((data.getUint8(17) & 0x1F) << 3 | (data.getUint8(18) & 0xE0) >> 5) / 255.0;
-
-		onStateChangeCallback( state );
+		onStateChangeCallback(state);
 
 	}
 
-	function onStateChangeCallback() {}
+	function onStateChangeCallback () { }
 
 	//
 
 	return {
 		connect: connect,
-		onStateChange: function ( callback ) {
+		onStateChange: function (callback) {
 			onStateChangeCallback = callback;
 		}
 	}
